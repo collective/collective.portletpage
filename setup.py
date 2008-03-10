3113376c85694b2242dc852a974a1ab5b0142294
optilude <optilude@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-03-10 20:05:17 +0000

    Tagging 1.0b2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.0b1'
+version = '1.0b2'
 
 setup(name='collective.portletpage',
       version=version,
@@ -27,7 +27,7 @@ view of the content object.
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
