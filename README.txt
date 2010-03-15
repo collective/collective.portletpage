0329d05a78251d1159f473f441807da59e376866
frisi <frisi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-15 14:41:08 +0000

    prepare release and include changelog in pkg-info

--- README.txt
+++ README.txt
@@ -1,9 +1,19 @@
-collective.portletpage Package Readme
-=====================================
+Overview
+========
+
+After installing this product, you will be able to add a "Portlet Page". This
+is a standard Plone "Page", but it also has a "Portlets" tab, from which you
+may assign portlets into four slots. The portlets will be shown on the main
+view of the content object.
 
 
-A Plone page that can contain portlets.
+Installation
+============
 
 Install it as normal, see http://plone.org/documentation/tutorial/buildout.
+
+
+Credits
+=======
 
 This work was generously sponsored by Centrepoint (http://centrepoint.org.uk)
--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup, find_packages
+import os
 
 version = '1.0b3'
 
 setup(name='collective.portletpage',
       version=version,
       description="A Plone page that can contain portlets",
-      long_description="""\
-After installing this product, you will be able to add a "Portlet Page". This
-is a standard Plone "Page", but it also has a "Portlets" tab, from which you
-may assign portlets into four slots. The portlets will be shown on the main
-view of the content object.
-""",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
