ee056cc6018c4adc56ba1419f102d00a5a003cb0
keul <keul@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-08-24 14:47:34 +0000

    support for z3c.autoinclude

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(name='collective.portletpage',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
-      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
